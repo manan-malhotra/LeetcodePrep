9275f3e2f82ff44d887e24bf544605859414d1fc
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-08 15:23:43 +0530

    9. Find All Numbers Disappeared in an Array

--- Arrays/FindAllNumbersDisappearedInAnArray.java
+++ Arrays/FindAllNumbersDisappearedInAnArray.java
@@ -1,3 +1,10 @@
+// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
+
+// Given an array nums of n integers where nums[i] is in the range [1, n], 
+// return an array of all the integers in the range [1, n] that do not appear in nums.
+
+// Tags: Cycle sort, HashMap, HashSet
+
 package Arrays;
 
 import java.util.ArrayList;
