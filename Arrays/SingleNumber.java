82b2c50a59d5dc401db74f57422b6b9129e3c111
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-08 15:41:17 +0530

    10. Single Number

--- Arrays/SingleNumber.java
+++ Arrays/SingleNumber.java
@@ -1,3 +1,12 @@
+// https://leetcode.com/problems/single-number/description/
+
+// Given a non-empty array of integers nums, every element appears twice except for one. 
+// Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Tags: XOR Bit Manipulation
+
 package Arrays;
 
 public class SingleNumber {
