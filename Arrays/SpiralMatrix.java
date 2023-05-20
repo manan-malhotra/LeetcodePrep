b42e4c09e6828b5738888b0f1e0c1b364af1b836
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-05-20 15:22:30 +0530

    19. Spiral Matrix

---
+++ Arrays/SpiralMatrix.java
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/spiral-matrix/description/
+
+// Given an m x n matrix, return all elements of the matrix in spiral order.
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [1,2,3,6,9,8,7,4,5]
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(spiralOrder(matrix));
+    }
+    public static List<Integer> spiralOrder(int[][] mat) {
+        // Define ans list to store the result.
+        List<Integer> ans = new ArrayList<>();
+                
+        int n = mat.length; // no. of rows
+        int m = mat[0].length; // no. of columns
+
+        // Initialize the pointers required for traversal.
+        int top = 0, left = 0, bottom = n - 1, right = m - 1;
+
+        // Loop until all elements are not traversed.
+        while (top <= bottom && left <= right) {
+
+            // For moving left to right
+            for (int i = left; i <= right; i++)
+                ans.add(mat[top][i]);
+
+            top++;
+
+            // For moving top to bottom.
+            for (int i = top; i <= bottom; i++)
+                ans.add(mat[i][right]);
+
+            right--;
+
+            // For moving right to left.
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    ans.add(mat[bottom][i]);
+
+                bottom--;
+            }
+
+            // For moving bottom to top.
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    ans.add(mat[i][left]);
+
+                left++;
+            }
+        }
+        return ans;
+    }
+}
