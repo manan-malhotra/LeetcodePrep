c04a2a283253ab36252fbca6ca67b9ce3e969d46
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-06-18 14:50:04 +0530

    03. Search in Rotated Sorted Array

--- BinarySearch/SearchInRotatedSortedArray.java
+++ BinarySearch/SearchInRotatedSortedArray.java
@@ -1,3 +1,14 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+// Example 1:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+// Example 2:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+
 package BinarySearch;
 
 public class SearchInRotatedSortedArray {
